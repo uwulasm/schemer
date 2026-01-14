bc35ad1dde355a0363c9c2409f255037d5f78f05
uwulasm <uwuboyfriendasmr@protonmail.com>
2026-01-14 16:30:03 +0100

    add builtin functions

--- src/parser.rs
+++ src/parser.rs
@@ -14,12 +14,14 @@ pub fn parse_sexpr<'a>() -> impl Parser<'a, &'a str, Sexpr> {
             .map(|x: &str| x.to_string())
             .or(one_of("+-*/").map(|x: char| x.to_string()));
 
-        let number = number::<RUST_LITERAL, &str, f32, _>().padded().map(|x: f32| Sexpr::Number(x));
-        let list = sexpr.repeated().collect::<Vec<_>>().delimited_by(just('(').padded(), just(')').padded()).map(Sexpr::List);
-        choice((
-                ident.map(|x| Sexpr::Ident(x)),
-                number,
-                list,
-        ))
+        let number = number::<RUST_LITERAL, &str, f32, _>()
+            .padded()
+            .map(|x: f32| Sexpr::Number(x));
+        let list = sexpr
+            .repeated()
+            .collect::<Vec<_>>()
+            .delimited_by(just('(').padded(), just(')').padded())
+            .map(Sexpr::List);
+        choice((ident.map(|x| Sexpr::Ident(x)), number, list))
     })
 }
--- src/vm.rs
+++ src/vm.rs
@@ -1,20 +1,43 @@
+use chumsky::prelude::todo;
 use std::collections::HashMap;
+
 use crate::parser::Sexpr;
 
 #[derive(Debug, Clone)]
 pub enum Value {
+    Function(Function),
     Number(f32),
     Nothing,
+    True,
+    False,
+}
+
+#[derive(Debug, Clone)]
+enum Function {
+    LangFunction(Sexpr),
+    Builtin(fn(&[Value]) -> Value),
+}
+
+#[derive(Debug, Clone)]
+struct Variable {
+    name: String,
+    value: Value,
 }
 
 pub struct VM {
     special_cases: HashMap<String, fn(&Self, &[Sexpr]) -> Value>,
+    variables: Vec<Variable>,
 }
 
 impl VM {
     pub fn new() -> VM {
         VM {
-            special_cases: HashMap::from([("+".to_string(), VM::add_special_case as fn(&Self, &[Sexpr]) -> Value)]),
+            //special_cases: HashMap::from([("+".to_string(), VM::add_special_case as fn(&Self, &[Sexpr]) -> Value)]),
+            special_cases: HashMap::new(),
+            variables: vec![Variable {
+                name: "+".to_string(),
+                value: Value::Function(Function::Builtin(VM::add_builtin)),
+            }],
         }
     }
 
@@ -27,6 +50,16 @@ impl VM {
         }
     }
 
+    fn add_builtin(values: &[Value]) -> Value {
+        let mut total = 0f32;
+        for value in values {
+            let Value::Number(x) = value else { todo!() };
+            total += x;
+        }
+
+        Value::Number(total)
+    }
+
     pub fn evaluate(&self, expr: &Sexpr) -> Value {
         match expr {
             Sexpr::Number(x) => Value::Number(*x),
@@ -37,6 +70,21 @@ impl VM {
                         Sexpr::Ident(x) => {
                             if let Some(&special_case) = self.special_cases.get(x) {
                                 special_case(self, list)
+                            } else if let Some(v) = self.variables.iter().find(|&v| *x == v.name) {
+                                match &v.value {
+                                    Value::Function(f) => {
+                                        let arguments = list
+                                            .iter()
+                                            .skip(1)
+                                            .map(|x| self.evaluate(x))
+                                            .collect::<Vec<_>>();
+                                        match f {
+                                            Function::Builtin(builtin) => builtin(&arguments),
+                                            _ => todo!(),
+                                        }
+                                    }
+                                    _ => todo!(),
+                                }
                             } else {
                                 todo!()
                             }
@@ -46,7 +94,7 @@ impl VM {
                 } else {
                     Value::Nothing
                 }
-            },
+            }
         }
     }
 }
